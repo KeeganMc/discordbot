b1084dbdb35419bf081e7ea381d3dcf721279c17
KeeganMc <47431359+KeeganMc@users.noreply.github.com>
2019-02-24 12:57:13 -0600

    Add files via upload

---
+++ discoerdbot.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import random
+from discord import Game
+
+
+Client = discord.client
+client = commands.Bot(command_prefix = '!')
+Clientdiscord = discord.Client()
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(game=Game(name='      '))
+    print('Ready, Freddy') 
+
+
+@client.event
+async def on_message(message):
+    if message.content == 'Hi':
+        await client.send_message(message.channel,'Hello.')
+    if message.content == '!1273':
+        em = discord.Embed(description='')
+        em.set_image(url='down the Rockefeller street ')
+        await client.send_message(message.channel, embed=em)
+    if ('heck') in message.content:
+       await client.delete_message(message)
+    if message.content.startswith('!memes'):
+        randomlist = ['https://cleanmemes.files.wordpress.com/2019/01/img_7510.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7511.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7512.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7513.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7514.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7474.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7478.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7241.jpg']
+        await client.send_message(message.channel,(random.choice(randomlist)))
+client.run('NTQ5MDgzODYwOTkwOTUxNDM2.D1O0ig.CYEABuixCsiG-31B66NJxWed0y4')
+                      
