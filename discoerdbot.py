180454809da8d1fb8a8750c936fe01ce0559d66b
KeeganMc <47431359+KeeganMc@users.noreply.github.com>
2019-02-24 17:26:36 -0600

    Update discoerdbot.py

--- discoerdbot.py
+++ discoerdbot.py
@@ -29,7 +29,6 @@ async def on_message(message):
     if ('heck') in message.content:
        await client.delete_message(message)
     if message.content.startswith('!memes'):
-        randomlist = ['https://cleanmemes.files.wordpress.com/2019/01/img_7510.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7511.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7512.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7513.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7514.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7474.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7478.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7241.jpg']
+        randomlist = ['https://cleanmemes.files.wordpress.com/2019/01/img_7510.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7511.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7512.jpg',https://cleanmemes.files.wordpress.com/2019/01/img_7513.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7514.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7474.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7478.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7241.jpg']
         await client.send_message(message.channel,(random.choice(randomlist)))
-client.run('NTQ5MDgzODYwOTkwOTUxNDM2.D1O0ig.CYEABuixCsiG-31B66NJxWed0y4')
-                      
+client.run('TOKEN')
